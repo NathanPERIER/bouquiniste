5586d4b20b32500c9511a315dd0647b157750059
Nathan PERIER <periernathan2000@gmail.com>
2022-10-14 23:17:48 +0200

    Created base methods for a `Source` object

--- sources/base.py
+++ sources/base.py
@@ -1,5 +1,15 @@
 
+from core.models import ReleaseEntry
+
+from typing import Sequence
+
+from datetime import date
+
+
 class Source :
 
-	def getEntries(self) :
-		pass
+	def getEntries(self, begin: date, end: date) -> "Sequence[ReleaseEntry]" :
+		raise NotImplementedError()
+
+	def refineEntry(self, entry: ReleaseEntry) :
+		raise NotImplementedError()
