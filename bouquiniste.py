fafc722b51908769aedeedfb2cddc724c223a30a
Nathan PERIER <periernathan2000@gmail.com>
2022-10-14 20:25:51 +0200

    Added base for the main script

--- bouquiniste.py
+++ bouquiniste.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt='%d/%m/%Y %H:%M:%S')
+
+
+def main() :
+	pass
+
+
+if __name__ == '__main__' :
+	main()
