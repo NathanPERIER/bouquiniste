259167a14f63137384fc43fb07d059681688cbe3
Nathan PERIER <periernathan2000@gmail.com>
2022-10-15 20:57:11 +0200

    Replaced sequence by iterable in the base `Source` class

--- sources/base.py
+++ sources/base.py
@@ -1,14 +1,14 @@
 
 from core.models import ReleaseEntry
 
-from typing import Sequence
+from typing import Iterable
 
 from datetime import date
 
 
 class Source :
 
-	def getEntries(self, begin: date, end: date) -> "Sequence[ReleaseEntry]" :
+	def getEntries(self, begin: date, end: date) -> "Iterable[ReleaseEntry]" :
 		raise NotImplementedError()
 
 	def refineEntry(self, entry: ReleaseEntry) :
