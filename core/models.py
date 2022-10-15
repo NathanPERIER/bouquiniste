cec1a813a30bb6771404cf90d6719ad62dc6d89b
Nathan PERIER <periernathan2000@gmail.com>
2022-10-15 20:57:46 +0200

    Added author and ISBN to a release, removed country of origin

--- core/models.py
+++ core/models.py
@@ -1,5 +1,13 @@
 
+from enum import Enum
 from datetime import date
+
+
+class PublicationStatus(Enum) :
+	FINISHED = 'finished'
+	ONGOING = 'ongoing'
+	UNKNOWN = 'unknown'
+
 
 class ReleaseEntry :
 	def __init__(self) :
@@ -8,10 +16,11 @@ class ReleaseEntry :
 		self.image: "str | None"  = None
 		self.number: "int | None" = None
 		self.editor: "str | None" = None
+		self.author: "str | None" = None
 		self.release: date
+		self.isbn: "str | None" = None
 		self.price: "str | None" = None
 		self.pages: "int | None" = None
-		self.status: "str | None" = None
-		self.pub_country: "str | None" = None
+		self.status: PublicationStatus = PublicationStatus.UNKNOWN
+		self.pub_status: PublicationStatus = PublicationStatus.UNKNOWN
 		self.pub_number: "int | None" = None
-		self.pub_status: "str | None" = None
