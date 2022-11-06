ca45ea96da566915e007fdee1ce343f40844748d
Nathan PERIER <periernathan2000@gmail.com>
2022-11-06 20:22:45 +0100

    Added parsing of the argument and call to the engine

--- bouquiniste.py
+++ bouquiniste.py
@@ -1,12 +1,43 @@
 #!/usr/bin/python3
 
+from core import engine
+
+import re
+import sys
 import logging
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt='%d/%m/%Y %H:%M:%S')
 
 
+def help(retcode: int) :
+	print(f"usage : {sys.argv[0]} [<n_months>m][<n_weeks>w][<n_days>d]")
+	sys.exit(retcode)
+
+
 def main() :
-	pass
+
+	args = sys.argv[1:]
+	if len(args) == 0 :
+		help(1)
+	
+	if args[0] in ['-h', '--help'] :
+		help(0)
+	
+	m = re.fullmatch(r'([0-9]+m)?([0-9]+w)?([0-9]+d)?', args[0])
+	if m is None :
+		print(f"Bad argument format : `{args[0]}`")
+		sys.exit(1)
+	nums = [
+		0 if n is None else int(n[:-1])
+		for n in m.groups()
+	]
+
+	begin = datetime.now().date()
+	end = begin + relativedelta(months=nums[0], weeks=nums[1], days=nums[2])
+
+	engine.run(begin, end)
 
 
 if __name__ == '__main__' :
