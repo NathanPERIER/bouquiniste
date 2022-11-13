cc888125b23bfa899a48799489e2910d8bcb3857
Nathan PERIER <periernathan2000@gmail.com>
2022-11-13 23:42:35 +0100

    Added database check before sending the notifications

--- core/engine.py
+++ core/engine.py
@@ -1,9 +1,12 @@
 
 from core import config
 from core.config import ConfiguredSource
+from database import SqliteConnection, initDatabase, getDatabaseConnection
+import database.queries
 
 import logging
 from datetime import date
+from sqlite3 import Cursor
 
 logger = logging.getLogger(__name__)
 
@@ -14,23 +17,29 @@ def run(begin: date, end: date) :
 	if len(sources) == 0 :
 		logger.warning('No sources found, exitting')
 		return
-	for src in sources :
-		try :
-			__b_processSource(src, begin, end)
-		except Exception as e :
-			for notifier in src.notifiers :
-				notifier.notifyError(e)
+	with getDatabaseConnection() as con :
+		cur = con.cursor()
+		initDatabase(cur)
+		for src in sources :
+			try :
+				__b_processSource(src, begin, end, cur)
+			except Exception as e :
+				logger.exception("Uncaught error during source processing")
+				for notifier in src.notifiers :
+					notifier.notifyError(e)
 			
 
-def __b_processSource(src: ConfiguredSource, begin: date, end: date) :
+def __b_processSource(src: ConfiguredSource, begin: date, end: date, cur: Cursor) :
 	entries = src.source.getEntries(begin, end)
 	filtered = [
 		x for x in entries
-		if x.title in src.list
+		if x.title in src.names
+		or x.manga_id in src.ids
 	]
-	# TODO database check
+	filtered = database.queries.filterRecordedEntries(cur, src.identifier, filtered)
 	info = src.source.getInfo()
 	for entry in filtered :
 		src.source.refineEntry(entry)
 		for notifier in src.notifiers :
 			notifier.notifyRelease(entry, info)
+		database.queries.recordEntry(cur, src.identifier, entry)
